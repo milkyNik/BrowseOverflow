6ed663bc206fc1f13860f474d8571e6a2d53486c
MilkyNik <milniklaus@gmail.com>
2018-08-30 21:43:47 +0300

    Creating project.

---
+++ BrowseOverflow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrowseOverflow
+//
+//  Created by iMac on 30.08.18.
+//  Copyright © 2018 hata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BrowseOverflow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BrowseOverflow
+//
+//  Created by iMac on 30.08.18.
+//  Copyright © 2018 hata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
